7f0d814621ca74a6f2ea1b24494f11bc01b406bc
danish-jamil <mrdanishjameel@gmail.com>
2018-07-11 20:11:49 +0500

    Internationalization: changed extension to xliff for POEditor
    By default angular creates xlf extension

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', pathMatch: 'full', component: HomeComponent },
+  { path: 'post', pathMatch: 'full', component: PostComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
